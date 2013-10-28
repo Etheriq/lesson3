b45745d97ced47ce56b44b61628e01a6e5323466
Yuriy Tarnavskiy <etheriq2@gmail.com>
2013-10-28 14:43:43 +0200

    rewrite model, fix index.php

--- src/invent/model/model.php
+++ src/invent/model/model.php
@@ -15,10 +15,9 @@ use invent\cls\Monitor;
 
 include "../../../vendor/autoload.php";
 
-function showAll()
+function pcInit()
 {
     $pc = array();
-
     $pc1= new Pc();
     $pc1->setInventoryNumber("44003010012356");
     $pc1->setCpu("Pentium 4 3 Ghz");
@@ -33,72 +32,73 @@ function showAll()
     $pc2->setRam("2 Gb");
     $pc[] = $pc2;
 
-    $printer1 = new Printer();
-    $printer1->setInventoryNumber("44003010025541");
-    $printer1->setModel("HP 1320");
-    $printer1->setType("Laser printer");
-    $printer1->setCartridge("Q5949A");
-    $pc[] = $printer1;
+    return $pc;
+}
 
+function monitorInit()
+{
+    $monitor = array();
     $mon1 = new Monitor();
     $mon1->setInventoryNumber("44003010032655");
     $mon1->setModel("Samsung 710n");
     $mon1->setType("TFT");
     $mon1->setInterf("D-SUB");
-    $pc[] = $mon1;
+    $monitor[] = $mon1;
 
-    return $pc;
+    $mon2 = new Monitor();
+    $mon2->setInventoryNumber("44003010036578");
+    $mon2->setModel("Asus BF17g");
+    $mon2->setType("TFT");
+    $mon2->setInterf("D-SUB");
+    $monitor[] = $mon2;
+
+    return $monitor;
+}
+
+function printerInit()
+{
+    $printer = array();
+
+    $printer1 = new Printer();
+    $printer1->setInventoryNumber("44003010025541");
+    $printer1->setModel("HP 1320");
+    $printer1->setType("Laser printer");
+    $printer1->setCartridge("Q5949A");
+    $printer[] = $printer1;
+
+    $printer2 = new Printer();
+    $printer2->setInventoryNumber("44003010025364");
+    $printer2->setModel("Canon LBP2900");
+    $printer2->setType("Laser printer");
+    $printer2->setCartridge("103A");
+    $printer[] = $printer2;
+
+    return $printer;
+}
+
+function showAll()
+{
+    $result = array();
+    $result[] = pcInit();
+    $result[] = monitorInit();
+    $result[] = printerInit();
+
+    return $result;
 }
 
 function showPc()
 {
-    $result = array();
-
-    $pc1= new Pc();
-    $pc1->setInventoryNumber("44003010012356");
-    $pc1->setCpu("Pentium 4 3 Ghz");
-    $pc1->setHdd("329 Gb");
-    $pc1->setRam("2 Gb");
-
-    $pc2 = new Pc();
-    $pc2->setInventoryNumber("44003010056955");
-    $pc2->setCpu("Core2Duo E2140 2.13 Ghz");
-    $pc2->setHdd("160 Gb");
-    $pc2->setRam("2 Gb");
-
-    $result[] = $pc1;
-    $result[] = $pc2;
-
-    return $result;
+    return pcInit();
 }
 
 function showMonitor()
 {
-    $result = array();
-
-    $mon1 = new Monitor();
-    $mon1->setInventoryNumber("44003010032655");
-    $mon1->setModel("Samsung 710n");
-    $mon1->setType("TFT");
-    $mon1->setInterf("D-SUB");
-    $result[] = $mon1;
-
-    return $result;
+    return monitorInit();
 }
 
 function showPrinter()
 {
-    $result = array();
-
-    $printer1 = new Printer();
-    $printer1->setInventoryNumber("44003010025541");
-    $printer1->setModel("HP 1320");
-    $printer1->setType("Laser printer");
-    $printer1->setCartridge("Q5949A");
-    $result[] = $printer1;
-
-    return $result;
-
+    return printerInit();
 }
 
 function showTest()
@@ -114,5 +114,6 @@ function showTest()
     $response = new Response();
     $response->setContent($pcInfo);
     $response->headers->set('Content-Type', 'text/html');
+
     return $response->send();
 }
